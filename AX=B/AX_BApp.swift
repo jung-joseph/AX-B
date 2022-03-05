459702fc4c7d7e9981a5709acf5e8486286a25cd
Joseph Jung <joseph.jung@icloud.com>
2022-03-05 16:28:34 -0700

    Initial Commit

---
+++ AX=B/AX_BApp.swift
@@ -0,0 +1,17 @@
+//
+//  AX_BApp.swift
+//  AX=B
+//
+//  Created by Joseph Jung on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AX_BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
